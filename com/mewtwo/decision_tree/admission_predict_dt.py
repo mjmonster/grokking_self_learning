eac5e553e98b8fafe4f0459afc6e3cbed7b5324a
MEWTWO <mewtwoji@gmail.com>
2025-11-09 23:36:57 +0800

    251109 - Decision Tree | Admission Predict using sklearn Predict using restricted dt with one dataset

--- com/mewtwo/decision_tree/admission_predict_dt.py
+++ com/mewtwo/decision_tree/admission_predict_dt.py
@@ -10,11 +10,13 @@ data = data.drop(['Chance of Admit'], axis=1)
 features = data.drop('Admitted', axis=1)
 labels = data['Admitted']
 
-# dt = DecisionTreeClassifier()
-
-dt = DecisionTreeClassifier(max_depth=3, min_samples_leaf=10, min_samples_split=10)
+dt = DecisionTreeClassifier()
 dt.fit(features, labels)
-
 print(dt.predict(features[0:5]))
+
+dt_restricted = DecisionTreeClassifier(max_depth=3, min_samples_leaf=10, min_samples_split=10)
+dt_restricted.fit(features, labels)
+#                             337,118,4,4.5,4.5,9.65,1,0.92
+print(dt_restricted.predict([[500,320,110,3,4.0,3.5,8.9,0]]))
 
 save_tree(dt)
